bdb8b9cfad91cb29bff53fc710c6c4ed63d8d3b6
imtiaz-ahmmed <imtiazahmmed00@gmail.com>
2025-05-21 02:01:03 +0600

    App Part Added

---
+++ src/Pages/Home/App.jsx
@@ -0,0 +1,35 @@
+import img1 from "/images/app.png";
+import img2 from "/images/play-store.png";
+import img3 from "/images/app-store.png";
+import { Link } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className="pt-8 mx-32 mb-12 px-6 bg-gradient-to-r from-[#53ed83] to-[#04a6f6] grid grid-cols-2 items-center justify-center rounded-2xl ">
+      <div>
+        <h2 className="text-2xl md:text-5xl text-black font-bold">
+          Download our mobile app,
+        </h2>
+        <h2 className="text-2xl md:text-5xl text-black font-bold">
+          {" "}
+          start learning today
+        </h2>
+        <div className="flex gap-4 items-center mt-8">
+          <Link to="/">
+            {" "}
+            <img className="rounded-2xl" src={img2} alt="" />
+          </Link>
+          <Link to="/">
+            {" "}
+            <img className="rounded-2xl" src={img3} alt="" />
+          </Link>
+        </div>
+      </div>
+      <div>
+        <img src={img1} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Banner from "./Banner";
 import About from "./About";
 import Features from "./Features";
+import App from "./App";
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-white">
       <Banner></Banner>
       <About></About>
       <Features></Features>
+      <App></App>
     </div>
   );
 };
