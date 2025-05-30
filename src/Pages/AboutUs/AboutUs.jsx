da9ed77173bc67f04dd213225fffd2c123a409f7
miraz-alvee <mirazalvee98@gmail.com>
2025-05-31 02:39:32 +0600

    about

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import image1 from "../../../public/images/about.jpeg";
 
 const AboutUs = () => {
   return (
-    <div>
-      <h2>About Us</h2>
+    <div className="bg-white">
+      <div class="relative">
+        <img src={image1} alt="Background" className="w-full h-auto" />
+        <div className="absolute inset-0 flex flex-col items-center justify-center px-4">
+          <h1 className="text-black text-5xl font-semibold">About Us</h1>
+          <p className="text-black text-xl mt-4">
+            Everything you need to simplify how work
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
